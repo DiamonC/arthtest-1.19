f8606b55c0618f3fa9a5d752e7884b2d653890d4
diamonc <diamoncyt@gmail.com>
2022-07-15 16:36:45 -0500

    registered CubeEntity

--- src/main/java/com/example/example_mod/ExampleMod.java
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -1,5 +1,12 @@
 package com.example.example_mod;
 
+import com.example.example_mod.entity.CubeEntity;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
@@ -10,7 +17,11 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
-
+	public static final EntityType<CubeEntity> CUBE = Registry.register (
+			Registry.ENTITY_TYPE,
+			new Identifier("examplemod", "cube"),
+			FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, CubeEntity::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
+	);
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
